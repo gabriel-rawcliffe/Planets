2bf3e306966395c7b907f0b3ae184510ee645d6e
Gabriel Rawcliffe <gabriel.rawcliffe@gmail.com>
2023-08-31 18:08:39 +1200

    functioning planet get route and routerprovider

--- client/apis/apiClient.ts
+++ client/apis/apiClient.ts
@@ -1,7 +1,7 @@
 import request from 'superagent'
 
-export async function getPlanet() {
-  const response = await request.get('/api/v1/planet')
+export async function getPlanet(planet: string) {
+  const response = await request.get(`/api/v1/planets/${planet}`)
 
   return response.body
 }
--- client/routes.tsx
+++ client/routes.tsx
@@ -1,12 +1,20 @@
-import { Route, createRoutesFromElements } from 'react-router-dom'
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom'
 
 import App from './components/App'
 import Home from './components/Home'
 import PlanetDetails from './components/PlanetDetails'
 
-export const routes = createRoutesFromElements(
-  <Route path="/" element={<App />}>
-    <Route index element={<Home />} />
-    <Route path="/planetdetails" element={<PlanetDetails />} />
-  </Route>
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index element={<Home />} />
+      <Route path="planetdetails" element={<PlanetDetails />} />
+    </Route>
+  )
 )
+
+export default router
