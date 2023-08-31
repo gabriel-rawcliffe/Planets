7a65be03fd8fd3279c169ff761bc866d82a5c02d
Cris Kiri <cris.kiri@gmail.com>
2023-09-01 10:56:41 +1200

    feat: planet other component

--- client/components/PlanetOther.tsx
+++ client/components/PlanetOther.tsx
@@ -1,3 +1,39 @@
+import { Params, useParams } from 'react-router-dom'
+import { useQuery } from '@tanstack/react-query'
+
+import { getPlanet } from '../apis/apiClient'
+import { Planet } from '../../models/planets'
+
 export default function PlanetOther() {
-  return <>Other Planet</>
+  // const { name } = useParams<Params>()
+  const name = 'Neptune'
+  const {
+    data: otherPlanet,
+    isLoading,
+    error,
+  } = useQuery(['planet', name], () => getPlanet(name))
+
+  console.log(otherPlanet)
+
+  if (isLoading) return <div>Loading...</div>
+
+  if (error) return <div>Oops! Something went wrong...</div>
+
+  return (
+    <>
+      <h2>{name}</h2>
+      <img src="#" alt="other-planet" />
+      <ul>
+        {otherPlanet?.map((planet: Planet) => (
+          <li key={planet.name}>Mass - {planet.mass}</li>
+        ))}
+        {otherPlanet?.map((planet: Planet) => (
+          <li key={planet.name}>Period - {planet.period}</li>
+        ))}
+        {otherPlanet?.map((planet: Planet) => (
+          <li key={planet.name}>Distance - {planet.distance_light_year}</li>
+        ))}
+      </ul>
+    </>
+  )
 }
--- server/routes/planets.ts
+++ server/routes/planets.ts
@@ -5,6 +5,7 @@ import 'dotenv/config'
 
 const router = express.Router()
 
+// GET /api/v1/planets/:planet
 router.get(`/:planet`, async (req, res) => {
   try {
     const planet = req.params.planet
@@ -13,6 +14,7 @@ router.get(`/:planet`, async (req, res) => {
       .get(`https://api.api-ninjas.com/v1/planets?name=${planet}`)
       .set({ 'X-Api-Key': process.env.X_API_Key })
     res.json(response.body)
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (error: any) {
     res.status(error.response.status || 500).json(error.response.body)
   }
