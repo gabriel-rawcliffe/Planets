045d9d0583312a4888c6132ead364e93963969e3
zoe <zoeannproudfoot123@gmail.com>
2023-09-01 10:56:10 +1200

    Added Planet Eart component

--- client/components/PlanetEarth.tsx
+++ client/components/PlanetEarth.tsx
@@ -1,3 +1,24 @@
+import { useQuery } from '@tanstack/react-query'
+import { Link } from 'react-router-dom'
+import { getPlanet } from '../apis/apiClient'
+
 export default function PlanetEarth() {
-  return <>Planet Earth</>
+  const { data, isLoading, isError } = useQuery(['getPlanet'], () =>
+    getPlanet('')
+  )
+  console.log(getPlanet)
+
+  if (isLoading) {
+    return <div>Planet Data Loading</div>
+  }
+  if (isError || !data) {
+    return <div>There has been an error</div>
+  }
+
+  return (
+    <>
+      <h1>Planet Earth</h1>
+      {data && <img src={data.imageUrl} alt="Planet Earth" />}
+    </>
+  )
 }
